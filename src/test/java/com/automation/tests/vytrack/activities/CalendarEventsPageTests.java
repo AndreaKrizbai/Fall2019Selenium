34c8e2046e979adfc35f69ac0733d72c7e7fbeda
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-03-17 21:52:55 -0400

    Calendar Events Tests

--- src/test/java/com/automation/tests/vytrack/activities/CalendarEventsPageTests.java
+++ src/test/java/com/automation/tests/vytrack/activities/CalendarEventsPageTests.java
@@ -12,6 +12,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class CalendarEventsPageTests {
     private By usernameBy = By.id("prependedInput");
     private By passwordBy = By.id("prependedInput2");
@@ -22,6 +26,13 @@ public class CalendarEventsPageTests {
     private String storeManagerPassword="UserUser123";
     private By activitiesBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Activities')]");
     private By createCalendarBtnBy = By.cssSelector("a[title='Create Calendar event']");
+    private By currentUserBy = By.cssSelector("#user-menu > a");
+    //private By ownerBy = By.id("s2id_oro_calendar_event_form_calendar");
+   // private By ownerBy = By.cssSelector("#s2id_oro_calendar_event_form_calendar");
+    private By ownerBy = By.className("select2-chosen");
+    private By titleBy = By.cssSelector("[name='oro_calendar_event_form[title]']");
+    private By startDateBy = By.cssSelector("[id*='date_selector_oro_calendar_event_form_start-uid']");
+    private By startTimeBy = By.cssSelector("[id*='time_selector_oro_calendar_event_form_start-uid']");
 
 
     @BeforeMethod
@@ -46,6 +57,30 @@ public class CalendarEventsPageTests {
         WebElement createCalendarBtnElement = driver.findElement(createCalendarBtnBy);
         Assert.assertTrue(createCalendarBtnElement.isDisplayed());
 
+    }
+
+    @Test(description = "Default options")
+    public void verifyDefaultValues(){
+        //click on create Calendar Event button
+        driver.findElement(createCalendarBtnBy).click();
+        BrowserUtils.wait(3);
+        //default owner name should be current user
+        String currentUserName = driver.findElement(currentUserBy).getText().trim();
+        String defaultOwnerName = driver.findElement(ownerBy).getText().trim();
+        Assert.assertEquals(currentUserName,defaultOwnerName);
+
+        //default title should be blank
+        WebElement titleElement = driver.findElement(titleBy);
+        Assert.assertTrue(titleElement.getAttribute("value").isEmpty());
+
+        //default start date should be current date
+        String expectedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+        String actualDate = driver.findElement(startDateBy).getAttribute("value");
+        Assert.assertEquals(actualDate,expectedDate);
+
+        String expectedTime = LocalTime.now().format(DateTimeFormatter.ofPattern("h:m a"));
+        String actualTime = driver.findElement(startTimeBy).getAttribute("value");
+        Assert.assertEquals(actualTime, expectedTime);
     }
 
     @AfterMethod
