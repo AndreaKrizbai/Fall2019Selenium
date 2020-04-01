f017de3e4a7a7f7d10a64af3cfc1b6b4a3e47782
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-03-31 22:04:15 -0400

    added changes and test reports

--- src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
@@ -3,17 +3,48 @@ package com.automation.tests.vytrack;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ExtentHtmlReporterConfiguration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
 
 public abstract class AbstractTestBase {
     //will be visible in the subclass, regardless on subclass location (same package or no)
     protected WebDriverWait wait;
     protected Actions actions;
+
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest test;
+
+    @BeforeTest
+    public void setupTest() {
+        report = new ExtentReports();
+        String reportPath = "";
+
+        //location of report file
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/report.html";
+        }
+
+        //is a HTML report itself
+        htmlReporter = new ExtentHtmlReporter(reportPath);
+        report.attachReporter(htmlReporter);
+        htmlReporter.config().setReportName("VYTRACK Test Automation Results");
+    }
+
 
     @BeforeMethod
     public void setup(){
@@ -24,10 +55,20 @@ public abstract class AbstractTestBase {
         actions = new Actions(Driver.getDriver());
     }
 
+    @AfterTest
+    public void afterTest(){
+        //to release a report
+        report.flush();
+    }
+
     @AfterMethod
-    public void teardown(ITestResult iTestResult){
+    public void teardown(ITestResult iTestResult) throws IOException {
         if(iTestResult.getStatus() == ITestResult.FAILURE){
-            BrowserUtils.getScreenshot(iTestResult.getName());
+            String screenShotPath = BrowserUtils.getScreenshot(iTestResult.getName());
+            test.fail(iTestResult.getName());//attach test name that failed
+            BrowserUtils.wait(2);
+            test.addScreenCaptureFromPath(screenShotPath,"Failed");//attach screenshot
+            test.fail(iTestResult.getThrowable());//attach console output
         }
         Driver.closeDriver();
     }
--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -11,12 +11,16 @@ public class NewLoginTests extends AbstractTestBase {
 
     @Test
     public void verifyPageTitle(){
+        //we have to add this line at the beginning of every test
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         //wait.until(ExpectedConditions.titleContains("Dashboard"));
         //Driver.getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         BrowserUtils.wait(3);
-        Assert.assertEquals(Driver.getDriver().getTitle(),"Dashboard");
+        Assert.assertEquals(Driver.getDriver().getTitle(),"Dash");
+        //if assertion passed, it will set test status in report to passed
+        test.pass("Page title Dashboard was verified");
     }
 
     @Test
